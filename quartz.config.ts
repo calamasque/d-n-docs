5c5d4874964de06357099bb61ca1ade2c0021867
Joost Broersen <joostbroersen@Joosts-MacBook-Air.local>
2025-07-21 12:59:52 +0200

    Update quartz config

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,14 +8,14 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "MetaData",
+    pageTitle: "De titel",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
       provider: "plausible",
     },
-    locale: "nl-nl",
+    locale: "en-US",
     baseUrl: "quartz.jzhao.xyz",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
