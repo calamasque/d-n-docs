7f2d22de66171bc4f3f49cd17295e7ade9262c5f
Joost Broersen <joostbroersen@Joosts-MacBook-Air.local>
2025-07-22 11:32:19 +0200

    Quartz sync: Jul 22, 2025, 11:32 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "MetaData"",
+    pageTitle: "MetaData",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
